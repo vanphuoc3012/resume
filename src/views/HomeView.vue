23b755ec9e9eabe688215faa59c44434187a9fca
vanphuoc <vanphuoc3012@gmail.com>
2023-06-25 14:44:42 +0700

    clean up

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,13 +3,8 @@
 </script>
 
 <template>
-  <div class="container mx-auto border">
-    <div class="name-container">
-      <h1 class="">Phuoc</h1>
-    </div>
-    <div>
+  <div >
 
-    </div>
   </div>
 </template>
 
