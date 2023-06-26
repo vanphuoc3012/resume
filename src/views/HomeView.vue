195412051862c4c9fdd1b26fdf2045830bb0c9cb
vanphuoc <vanphuoc3012@gmail.com>
2023-06-26 08:42:40 +0700

    icon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,17 +9,17 @@
       <img src="../assets/images/av.png" alt="Avatar">
       <div class="px-4">
         <h1 class="font-bold text-3xl ">Ho Van Phuoc</h1>
-        <p>Java Developer</p>
+        <p>Junior Developer</p>
       </div>
     </div>
 
     <!-- Link   -->
-    <div class="flex">
+    <div class="flex items-center px-4">
       <a href="https://github.com/vanphuoc3012"
          class="flex items-center px-1 hover:bg-gray-100 rounded-md"
          target="_blank" rel="noopener noreferrer"
       >
-        <img class="h-5 px-1" src="./../assets/images/github.svg" alt=""/>
+        <img class="h-5 px-1" src="./../assets/images/Github.svg" alt=""/>
         Developer
       </a>
       <span>/</span>
@@ -27,10 +27,35 @@
          class="flex items-center px-1 text-blue-500 hover:bg-gray-100 rounded-md"
          target="_blank" rel="noopener noreferrer"
       >
-        <img class="h-5 px-1" src="./../assets/images/linkedin.svg" alt=""/>
+        <img class="h-5 px-1" src="./../assets/images/LinkedIN.svg" alt=""/>
         Linkedin
       </a>
       <span>/</span>
+
+      <a href="mailto:vanphuoc3012@gmail.com?"
+         class="flex items-center px-1 text-red-400 hover:bg-gray-100 rounded-md"
+         target="_blank" rel="noopener noreferrer"
+      >
+        <img class="h-8 px-1" src="./../assets/images/Gmail.svg" alt=""/>
+        vanphuoc3012@gmail.com
+      </a>
+      <span>/</span>
+      <a href="tel:+843444415562"
+         class="flex items-center px-1 text-blue-500 hover:bg-gray-100 rounded-md"
+         target="_blank" rel="noopener noreferrer"
+      >
+        <i class="fa-solid fa-mobile-screen-button px-1"></i>
+        +84 344 441 5562
+      </a>
+
+    </div>
+
+    <div class="w-full pt-8 flex items-center px-4">
+      <p class="font-bold text-xl">Skill</p>
+      <hr class="ms-4 border-gray-600 border-2 w-full border-opacity-30"/>
+      <div class="">
+        Java
+      </div>
     </div>
   </div>
 </template>
